448c6a852f3d8354a1ac2d143be8e2bccea95a5b
Riley Stratton <riley.s.stratton@outlook.com>
2022-09-15 16:58:18 -0600

    started work

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,4 +1,3 @@
-
 const { MongoClient } = require("mongodb");
 const connectionString = process.env.ATLAS_URI;
 const client = new MongoClient(connectionString, {
@@ -9,11 +8,39 @@ const client = new MongoClient(connectionString, {
 let dbConnection;
 
 module.exports = {
-  connectToServer: function (callback) {
-    // Implement Database connection
+  connectToServer: function(callback) {
+    client.connect(function(err, db) {
+      if (err || !db) {
+        return callback(err);
+      }
+
+      dbConnection = db.db("sample_airbnb");
+      console.log("Successfully connected to MongoDB.");
+
+      return callback();
+    });
   },
 
-  getDb: function () {
+  getDb: function() {
     return dbConnection;
   },
 };
+
+const express = require("express");
+const recordRoutes = express.Router();
+
+recordRoutes.route("/listings").get(async function(req, res) {
+  const dbConnect = dbo.getDb();
+
+  dbConnect
+    .collection("listingsAndReviews")
+    .find({}).limit(50)
+    .toArray(function(err, result) {
+      if (err) {
+        res.status(400).send("Error fetching listings!");
+      } else {
+        res.json(result);
+      }
+    });
+});
+
