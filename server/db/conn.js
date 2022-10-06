2fd3338661a64f83e0185c3207c02f973e16ecfa
Riley Stratton <riley.s.stratton@outlook.com>
2022-10-06 14:21:02 -0600

    tried to connect

--- server/db/conn.js
+++ server/db/conn.js
@@ -31,6 +31,7 @@ const recordRoutes = express.Router();
 
 recordRoutes.route("/listings").get(async function(req, res) {
   const dbConnect = dbo.getDb();
+  console.log(dbConnect)
 
   dbConnect
     .collection("listingsAndReviews")
@@ -44,3 +45,60 @@ recordRoutes.route("/listings").get(async function(req, res) {
     });
 });
 
+recordRoutes.route("/listings/recordSwipe").post(function (req, res) {
+  const dbConnect = dbo.getDb();
+  console.log(dbConnect)
+
+  const matchDocument = {
+    listing_id: req.body.id,
+    last_modified: new Date(),
+    session_id: req.body.session_id,
+    direction: req.body.direction
+  };
+
+  dbConnect
+    .collection("matches")
+    .insertOne(matchDocument, function (err, result) {
+      if (err) {
+        res.status(400).send("Error inserting matches!");
+      } else {
+        console.log(`Added a new match with id ${result.insertedId}`);
+        res.status(204).send();
+      }
+    });
+});
+
+recordRoutes.route("/listings/updateLike").post(function (req, res) {
+  const dbConnect = dbo.getDb();
+  const listingQuery = { _id: req.body.id };
+  const updates = {
+    $inc: {
+      likes: 1
+    }
+  };
+
+  dbConnect
+    .collection("listingsAndReviews")
+    .updateOne(listingQuery, updates, function (err, _result) {
+      if (err) {
+        res.status(400).send(`Error updating likes on listing with id ${listingQuery.id}!`);
+      } else {
+        console.log("1 document updated");
+      }
+    });
+});
+
+recordRoutes.route("/listings/delete/:id").delete((req, res) => {
+  const dbConnect = dbo.getDb();
+  const listingQuery = { listing_id: req.body.id };
+
+  dbConnect
+    .collection("listingsAndReviews")
+    .deleteOne(listingQuery, function (err, _result) {
+      if (err) {
+        res.status(400).send(`Error deleting listing with id ${listingQuery.listing_id}!`);
+      } else {
+        console.log("1 document deleted");
+      }
+    });
+});
